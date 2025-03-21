40fff5d539e468d27f5b286f155c00746a29f603
Dan Jakob Ofer <dan@ofer.to>
2025-03-21 12:30:07 -0400

    chore(rectangle): draw moveable rectangle

--- Snake.sdl/Snake.sdl/hello.c
+++ Snake.sdl/Snake.sdl/hello.c
@@ -9,12 +9,20 @@
   including commercial applications, and to alter it and redistribute it
   freely.
 */
+#include "SDL3/SDL_keycode.h"
+#include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_stdinc.h"
+
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
+
+static float right_movement_span = 0;
+static SDL_FRect frect = {.x = 50, .y = 100, .w = 20, .h = 30};
 
 /* This function runs once at startup. */
 SDL_AppResult
@@ -24,15 +32,53 @@ SDL_AppInit(void** appstate, int argc, char* argv[]) {
     SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
   return SDL_APP_CONTINUE;
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult
 SDL_AppEvent(void* appstate, SDL_Event* event) {
-  if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT) {
+  if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_Q) {
     return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
   }
+
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+  }
+
+  if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_L) {
+    if (frect.x >= 340) {
+      return SDL_APP_CONTINUE;
+    }
+
+    frect.x += 10.0;
+  }
+
+  if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_K) {
+    if (frect.y <= 0) {
+      return SDL_APP_CONTINUE;
+    }
+
+    frect.y -= 10.0;
+  }
+
+  if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_H) {
+    if (frect.x <= 0 + 10) {
+      return SDL_APP_CONTINUE;
+    }
+
+    frect.x -= 10.0;
+  }
+
+  if (event->type == SDL_EVENT_KEY_DOWN && event->key.key == SDLK_J) {
+    if (frect.y >= (600 - 200) / 2.0) {
+      return SDL_APP_CONTINUE;
+    }
+
+    frect.y += 10.0;
+  }
+
   return SDL_APP_CONTINUE;
 }
 
@@ -50,11 +96,20 @@ SDL_AppIterate(void* appstate) {
   x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
   y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
+  /* const SDL_Rect rect = {.x = 50, .y = 100, .w = 20, .h = 30}; */
+  /* SDL_RenderRect(renderer, &frect); */
+
+  char rectange_x_location[3], rectange_y_location[3];
+  SDL_itoa((int)frect.x, rectange_x_location, 10);
+  SDL_itoa((int)frect.y, rectange_y_location, 10);
   /* Draw the message */
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
   SDL_RenderDebugText(renderer, x, y, message);
+  SDL_RenderFillRect(renderer, &frect);
+  SDL_RenderDebugText(renderer, x, y + 10, rectange_x_location);
+  SDL_RenderDebugText(renderer, x, y + 20, rectange_y_location);
   SDL_RenderPresent(renderer);
 
   return SDL_APP_CONTINUE;
